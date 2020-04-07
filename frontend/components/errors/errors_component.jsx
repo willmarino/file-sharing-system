5876dbcce779e514b47d651bc629fc8e2e3877a9
Will <w.marino997@gmail.com>
2020-04-07 15:31:26 -0400

    polish

--- frontend/components/errors/errors_component.jsx
+++ frontend/components/errors/errors_component.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const createErrorsComponent = (errors, inProp) => {
-  return <TransitionGroup>
-    <ul className='errors-list'>
+  return <ul className='errors-list'>
+    <TransitionGroup>
       {errors.map((error, idx) => (
         <CSSTransition key={idx} classNames='errors-transition' in={inProp} timeout={1500}>
           <li><p>{error}</p></li>
         </CSSTransition>
       ))}
-    </ul>
-  </TransitionGroup>
+    </TransitionGroup>
+  </ul>
 }
 
 export default createErrorsComponent;
